36ae61e9eda42033ebba45361cbb7c4e08f58bec
Matthew OTousa <mlotousa@gmail.com>
2020-02-29 09:52:32 -0800

    cleaned up unused

--- src/flags.js
+++ src/flags.js
@@ -1,12 +1,9 @@
-// functions that don't depend on the driver
 const wasBefore = (bootDate, { ts }) => bootDate > ts.$date
-// const fromBot = message => Object.hasOwnProperty(message, 'bot')
 const fromBot = message => 'bot' in message
 
 export default (bot, message, messageOptions, lastUpdate) => ({
   fromSelf: bot.id === message.u._id,
   isMentioned: message.mentions.map(m => m._id).some(id => id === bot.id),
-  // TODO test this
   spokenOf: message.msg.toLowerCase().includes(bot.username.toLowerCase()),
   fromBot: fromBot(message),
   notInRoom: !messageOptions.roomParticipant,
--- src/helpString.js
+++
@@ -1,8 +0,0 @@
-const printArgOption = ([arg, message]) => `
-${arg.padStart(20)} : ${message}`
-
-export default (usageStr, optionMap) =>
-  '```\n' + `\
-  usage: ${usageStr}
-  ${optionMap.map(o => printArgOption(o)).join('')} \
-  ` + '\n```'
